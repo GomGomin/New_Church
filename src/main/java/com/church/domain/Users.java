395b7065a0457fc394833f9dcc4820fa7b8ea1b9
pjione <a41924250@gmail.com>
2023-04-06 18:30:19 +0900

    add schedule

---
+++ src/main/java/com/church/domain/Users.java
@@ -0,0 +1,13 @@
+package com.church.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Users {
+	
+	private int uno;
+	private String id, password, name, email, tel;
+	
+}
